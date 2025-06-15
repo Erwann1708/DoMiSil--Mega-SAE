c1f0ff88951345eecbd81c597529b325c8361081
E10uan <elouanc51@gmail.com>
2025-06-15 03:03:54 +0200

    Création de l'interface fxml de la page d'acceuil de l'application (pas fini)

--- src/main/java/equipe5MegaSae/main/FestivalApp.java
+++ src/main/java/equipe5MegaSae/main/FestivalApp.java
@@ -3,6 +3,7 @@ package equipe5MegaSae.main;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -11,8 +12,9 @@ public class FestivalApp extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(FestivalApp.class.getResource("/equipe5MegaSae/view/acceuil.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        Scene scene = new Scene(fxmlLoader.load(), 1404, 740);
+        scene.setFill(Color.GRAY);
+        stage.setTitle("DoMiSi'l ManagerApp");
         stage.setScene(scene);
         stage.show();
     }
